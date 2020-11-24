8c8deccaca8aa789619e36fd1677852fddc0f1fd
Camara-Diadie <camara-diade@hotmail.fr>
2020-11-24 17:07:01 +0100

    voir pour $requeste

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Connection;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class HomeController extends AbstractController
 {
-    public function homepage(ResponseInterface $response)
+    public function homepage(ResponseInterface $response, ServerRequestInterface $request)
     {
-        return $this->template($response, 'homepage.html.twig');
+        
+        
+        return $this->template($response, 'home.html.twig');
     }
+
+    public function download(ResponseInterface $response, int $id)
+    {
+        $response->getBody()->write(sprintf('Identifiant: %d',$id));
+        return $response;
+    }
+    
 }
